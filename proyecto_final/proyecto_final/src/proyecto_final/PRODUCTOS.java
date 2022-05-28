b66192eea6011846c17ae2f7b10122d5bd31fec9
octavio9912 <78389955+octavio9912@users.noreply.github.com>
2022-05-28 15:41:21 -0500

    Update PRODUCTOS.java

--- proyecto_final/proyecto_final/src/proyecto_final/PRODUCTOS.java
+++ proyecto_final/proyecto_final/src/proyecto_final/PRODUCTOS.java
@@ -38,7 +38,7 @@ public class PRODUCTOS  extends JFrame {
 	 * Create the frame.
 	 */
 	public PRODUCTOS() {
-		setTitle("PRODUCTS");
+		setTitle("PRODUCTOS");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
