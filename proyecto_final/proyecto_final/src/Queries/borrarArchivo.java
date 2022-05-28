11290ea561307bafb5a77c3d7d91cada84259721
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2022-05-28 16:12:23 -0500

    borrarArchivo

---
+++ proyecto_final/proyecto_final/src/Queries/borrarArchivo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Queries;
+
+/**
+ *
+ * @author Desk-strako
+ */
+public class borrarArchivo {
+    //borrar archivo porfavor
+}
